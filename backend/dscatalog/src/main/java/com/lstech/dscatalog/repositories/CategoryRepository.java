fb2134434a8ba8c2c2392ab3718b384df745b6d6
flaviofontes29 <flavio.carvalho.fontes@gmail.com>
2022-09-21 23:06:49 -0300

    H2 database

---
+++ backend/dscatalog/src/main/java/com/lstech/dscatalog/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.lstech.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lstech.dscatalog.entities.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
---
+++ backend/dscatalog/src/main/java/com/lstech/dscatalog/services/CategoryService.java
@@ -0,0 +1,23 @@
+package com.lstech.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lstech.dscatalog.entities.Category;
+import com.lstech.dscatalog.repositories.CategoryRepository;
+
+
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+
+}
